9fa53b4b62ed2e4c871b7d2bfc3605049e4ef1b9
Narek <narek.tarverdyan@bk.ru>
2016-12-08 14:46:44 +0300

    cmr

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
-	$('.select').selectric();
+(function($) {
 
+    /*
+     * Spawn optionlift api client
+     * olApi(apiUrl, tradingPlatformUrl)
+     */
+    var olSdkClient = new olSdk('//office.binaring.com/api/', 'https://binaring.com/trading');
+    
+    olSdkClient.connectRegistrationForm({
+        '$form'            : $('#open-account-form'),
+        '$phone_prefix'    : $('#open-account-form input[name="phone_prefix"]'),
+        '$country'         : $('#open-account-form select[name=country]'),
+        'appendCountries'  : true,
+        'detectCountry'    : true
+    });
 
-	
-
-});
+})(jQuery);
